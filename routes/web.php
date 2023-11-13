bcbefaec9c9900f8e35399d8ca2bffbc1ea76cbf
Indra.P <114041587+X-Indra-P@users.noreply.github.com>
2023-11-13 12:45:27 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MahasiswaController;
 
 Route::resource('mahasiswa', MahasiswaController::class);
-
+Route::get('/', function () {
+    return redirect('/mahasiswa');
+} );
